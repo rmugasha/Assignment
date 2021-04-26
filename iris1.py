a6107b94c7b017f3a90e617f177718147c3d1b30
Rodney Mugasha <rod.mugasha@gmail.com>
2021-04-26 21:40:35 +0300

    scatter and pair plots

--- iris1.py
+++ iris1.py
@@ -2,9 +2,16 @@ import requests, csv
 import pandas as pd
 import numpy as np
 import statistics
+import seaborn as sns
 
 response = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 response.columns = ["sepal width","sepal length","petal length","petal width", "flower"]
 
 response.head(10)
+
+response.describe()
+
+sns.scatterplot(data=response, x = "petal length" , y= "petal width")
+
+sns.pairplot(response)
 
