b43fcd54c9eb78df7f663460ebfcdd92ac431d12
Rodney Mugasha <rod.mugasha@gmail.com>
2021-04-26 19:01:45 +0300

    response1 to response

--- iris1.py
+++ iris1.py
@@ -2,6 +2,8 @@ import requests, csv
 import pandas as pd
 import numpy as np
 
-response1 = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
-print(response1.head())
+response = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+
+
+print(response.head())
 
