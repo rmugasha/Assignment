d1c1421d4dc554fcac2e53ad47be45879cddb353
Rodney Mugasha <rod.mugasha@gmail.com>
2021-04-26 20:42:03 +0300

    added head()

--- iris1.py
+++ iris1.py
@@ -1,6 +1,7 @@
 import requests, csv
 import pandas as pd
 import numpy as np
+import statistics
 
 response = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 response.columns = ["sepal width","sepal length","petal length","petal width", "flower"]
