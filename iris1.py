327500b7aeff57c7c87d257e462c1cd2c9bcbbf6
Rodney Mugasha <rod.mugasha@gmail.com>
2021-04-26 18:10:07 +0300

    Added import numpy

--- iris1.py
+++ iris1.py
@@ -1,5 +1,7 @@
 import requests, csv
 import pandas as pd
+import numpy as np
 
 response1 = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 print(response1.head())
+
