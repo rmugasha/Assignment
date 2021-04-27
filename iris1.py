96c60fce4b72ac44d6a9cdee58722177025cfa7e
Rodney Mugasha <rod.mugasha@gmail.com>
2021-04-28 00:22:32 +0300

    added dropna()

--- iris1.py
+++ iris1.py
@@ -6,7 +6,8 @@ import seaborn as sns
 
 response = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 response.columns = ["sepal width","sepal length","petal length","petal width", "flower"]
-
+response = response.dropna()
+response = pd.DataFrame(data = response, columns = ["sepal width","sepal length","petal length","petal width"])
 response.head(10)
 
 response.describe()
