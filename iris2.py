a637b3527a22e728a19cecc4267894a7c97276cb
Rodney Mugasha <rod.mugasha@gmail.com>
2021-05-02 13:25:38 +0300

    changed DataFrame to iris

--- iris2.py
+++ iris2.py
@@ -4,9 +4,9 @@ import numpy as np
 import statistics
 import seaborn as sns
 
-response = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
-response.columns = ["sepal width","sepal length","petal length","petal width", "flower"]
-response = response.dropna()
-response = pd.DataFrame(data = response, columns = ["sepal width","sepal length"])
-response = response.head(6)
-sns.scatterplot(data = response, x = "sepal width", y = "sepal length")
+iris = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+iris.columns = ["sepal width","sepal length","petal length","petal width", "flower"]
+iris = iris.dropna()
+iris = pd.DataFrame(data = iris, columns = ["sepal width","sepal length"])
+iris = iris.head(6)
+sns.scatterplot(data = iris, x = "sepal width", y = "sepal length")
